5fd7038fa0947094193acd1330e20a1b8031026a
llf <fifidemacici@gmail.com>
2025-04-25 17:12:40 +0200

    update

--- pdf_utils.py
+++ pdf_utils.py
@@ -5,15 +5,22 @@ from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA256
 import base64
 import segno
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PASS = os.getenv('KEY')
+print(PASS)
 
 
-def encrypt_aes(inputpath, outputpath, password):
+def encrypt_aes(inputpath, outputpath):
     with open(inputpath, 'rb') as file:
         pdf_data = file.read()
     salt = get_random_bytes(16)
     # Longueur de la clé (32 bytes pour AES-256)
     # specifier SHA256 pour eviter le probleme du dechiffrement a cause de divergence d'algo
-    key = PBKDF2(password, salt, dkLen=32, count=1000000, hmac_hash_module=SHA256)
+    key = PBKDF2(PASS, salt, dkLen=32, count=1000000, hmac_hash_module=SHA256)
     # initial vector of 16-byte
     iv = get_random_bytes(16)
     # Create cipher, CBC requires a multiple of block size(16 bytes)
@@ -32,7 +39,7 @@ def encrypt_aes(inputpath, outputpath, password):
     with open(outputpath, 'w') as file:
         file.write(encrypted_base64)
 
-def dycrpt_aes(inputpath, outputpath, password):
+def dycrpt_aes(inputpath, outputpath):
     with open(inputpath, 'rb') as file:
         encrypted_base64 = file.read()
     # b64decode: Décode un objet octet-compatible ou une chaîne de caractères ASCII s encodée
@@ -42,7 +49,7 @@ def dycrpt_aes(inputpath, outputpath, password):
     iv = final_data[16:32]
     encrypted_data = final_data[32:]
     # derive key
-    key = PBKDF2(password, salt, dkLen=32,count=1000000, hmac_hash_module=SHA256)
+    key = PBKDF2(PASS, salt, dkLen=32,count=1000000, hmac_hash_module=SHA256)
     cipher = AES.new(key, AES.MODE_CBC, iv)
     padded_data = cipher.decrypt(encrypted_data)
     if len(padded_data) == 0:
@@ -55,9 +62,8 @@ def dycrpt_aes(inputpath, outputpath, password):
         file.write(pdf_data)
     return "Déchiffrement réussi"
 
-# encrypt_aes('./catcv.pdf', './catcv_encrypted.pdf','password')
-# encrypt_aes('./catcv.pdf','./catcv_aes.pdf', 'password')
-# dycrpt_aes('./catcv_aes.pdf','./catcv_de.pdf', 'password')
+# encrypt_aes('./catcv.pdf','./catcv_aes.pdf')
+# dycrpt_aes('./catcv_aes.pdf','./catcv_de.pdf')
 
 def qr_maker(link, filename):
     qr = segno.make(link)
